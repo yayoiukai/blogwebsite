18556915718a234b813e9c9745a267a3fc522ef5
Yayoi <yayoi.ukai@gmail.com>
2013-12-30 18:29:05 -0800

    change contents

--- documentary.py
+++ documentary.py
@@ -66,48 +66,49 @@ form="""<html>
             <div class="collapse navbar-collapse" id="collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#">Documentary</a></li>
-                    <li><a href="#">Education</a></li>
-                    <li><a href="#">Culture</a></li>
-                    <li><a href="#">About</a></li>
+                    <li><a href="#">Interview Prep</a></li>
+                    <li><a href="#">Interviews</a></li>
+                    <li><a href="#">Project</a></li>
+                    <li><a href="#">Women in Tech</a></li>
                 </ul>
             </div>
          </nav>
     </div>
     <div class="jumbotron" style="background-color:white;">
         <img>
-        <h2><i>Documentary, Education, and Culture</i></h2>
-        <p>Hi, My name is <b>Yayoi Ukai</b>. I am a <b>Software Engineer</b>. I made this website to share some of my thoughts.
-        I plan to write about documentaries I watched (huge fan of documentaries.) and some education issues/culture \
-         issues I am interested in. I hope this site is something interesting or useful. </p>
+        <h2><i>Bay Area Tech Girl</i></h2>
+        <p>Hi, My name is <b>Yayoi Ukai</b>. I am a Software Engineer. I studied computer science and engineering at UC Davis and \
+        currently I live in San Francisco and looking for job. \
+        This is my work in progress website and a blog. I put an interesting technical bits and pieces on this website.\
+        I hope this site is something interesting or useful. </p>
         <h3>Welcome!</h3>
 
     </div>
     <div class="row">
     <div class="col-md-3 col-sm-6">
-    <h4><span class="glyphicon glyphicon-film"></span>Documentary</h4>
-    <p>I watch a lot of documentaries every week. My favorite genres are politics, social issues, technology, and sciences \
-    My recent favorate was "Park Avenue", "1%","flaw", and "Davis vs Monsato". I write my reviews of each documentary I watched here.</p>
+    <h4><span class="glyphicon glyphicon-film"></span>Interview Prep</h4>
+    <p>As I am looking for a job, it is super important to be able to do well on the interviews. \
+    All my codes are in github but this section has an update for my interivew prep. </p>
     <p>Read More >></p>
     </div>
     <div class="col-md-3 col-sm-6" style="background-color:#FFFF91;">
-     <h4><span class="glyphicon glyphicon-book"></span>Education</h4>
-     <p>I am always interested in learning new things. I also think that our higher education system is deeply in danger. \
-     California school system went through hugh tuition hike and budget cut and student loans are big issues too. \
-     I write about anything related to learning and education here.
+     <h4><span class="glyphicon glyphicon-book"></span>Interviews</h4>
+     <p>I am always interested in what other fellow Software Engineers are doing. So I decided to interview them. \
+     read my interview correction here.</p>
      <p>Read More >></p>
      </p>
      </div>
      <div class="col-md-3 col-sm-6" style="background-color:#B8FF94;">
-     <h4><span class="glyphicon glyphicon-pencil"></span>Culture</h4>
-     <p>I am originally from Japan and I live in the U.S. more than 10 years now. I love languages, traveling and cooking. \
-     So I write about anything that is related to culture and languages. </p>
+     <h4><span class="glyphicon glyphicon-pencil"></span>Projects </h4>
+     <p>This is the section that slightly bigger project that I am working on \
+     such as this website. Or I am also working on maze generator and solver. So any update \
+     on my projects goes here. 
+      </p>
      <p>Read More >></p>
      </div>
-      <div class="col-md-3 col-sm-6" style="background-color:#FFB2D1;">
-     <h4><span class="glyphicon glyphicon-star" ></span>About</h4>
-     About me and this website. You can read about me more or you can also read about construct of this website. This is twitter bootstrap
-     and google app engine backend (Python).
+    <div class="col-md-3 col-sm-6" style="background-color:#FFB2D1;">
+     <h4><span class="glyphicon glyphicon-star" ></span>Women in TECH</h4>
+     Anything related to women in tech. 
      <p>Read More >></p>
      </div>
      </div>
